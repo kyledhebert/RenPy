e07061894e72775da005fabb04b1e854d27e09e7
Kyle Hebert <kyle.hebert@gmail.com>
2016-01-25 18:21:29 -0600

    added order, orderitem, category models

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Product(models.Model):
 	product_name = models.CharField(max_length=200)
+	# these should be taken from the category table
+	# in the database,but I'm sure how to do that just yet
 	PRODUCT_CATEGORY_CHOICES = (
 		("WEAPON", "Weapon"),
 		("ARMOR", "Armor"),
@@ -11,15 +14,46 @@ class Product(models.Model):
 										choices=PRODUCT_CATEGORY_CHOICES)
 
 	PRODUCT_SIZE_CHOICES = (
+		("N", "None"),
 		("S", "Small"),
 		("M", "Medium"),
 		("L", "Large")
 	)
 	product_size = models.CharField(max_length=1,
-									choices=PRODUCT_SIZE_CHOICES)
+									choices=PRODUCT_SIZE_CHOICES,
+									default = "N")
 	product_quantity = models.IntegerField()
 	product_cost = models.FloatField()
 
 
 	def __str__(self):
 		return self.product_name
+
+
+class Category(models.Model):
+	category_name = models.CharField(max_length=30)
+
+	class Meta:
+		verbose_name_plural = "Categories"
+
+
+class Order(models.Model):
+	order_date = models.DateField(auto_now_add=True)
+	order_total = models.FloatField()
+
+
+class OrderItem(models.Model):
+	"""Represents and item that has been sold"""
+	order_number = models.IntegerField()
+	product_name = models.CharField(max_length=200)
+	product_cost = models.FloatField()
+	quantity_sold = models.IntegerField()
+
+
+	class Meta:
+		verbose_name_plural = "Order Items"
+
+
+
+				
+								
