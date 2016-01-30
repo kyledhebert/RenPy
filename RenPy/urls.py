80e6ff8857d1e55d244cd52d7daf1f14e6bc618e
Kyle Hebert <kyle.hebert@gmail.com>
2016-01-30 12:32:31 -0600

    polished admin interface, updated navigation

--- RenPy/urls.py
+++ RenPy/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
 	url(r'^$', 'products.views.index', name='home'),
 	url(r'^about/$',
--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Product, Category, Order, OrderItem
 
+class ProductAdmin(admin.ModelAdmin):
+	model = Product
+	list_display = ('product_name',
+					'product_size', 'product_quantity',
+					'product_cost',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+	model = Order
+	list_display = ('order_date', 'order_total',)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+	model = OrderItem
+	list_display = ('order_number', 'product_name',
+					'product_cost', 'quantity_sold',)	
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
